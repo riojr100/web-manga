c7bf1368132b9e6d0c9b64a5bbba0c0df773937e
riojr100 <riojr100@gmail.com>
2023-05-24 23:52:13 +0700

    yes

--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -1,12 +1,12 @@
-// import React from "react";
-// import AboutUs from "../component/cardAboutUs";
+import React from "react";
+import AboutUs from "../component/cardAboutUs";
 
-// const About = () => {
-//   return (
-//     <div className="mainAbout">
-//       <AboutUs />
-//     </div>
-//   );
-// };
+const About = () => {
+  return (
+    <div className="mainAbout">
+      <AboutUs />
+    </div>
+  );
+};
 
-// export default About;
+export default About;
