9226e346772fb34b2ca36905724167e95e74e2e7
riojr100 <riojr100@gmail.com>
2023-05-26 12:36:07 +0700

    yes

--- src/component/searchCard.jsx
+++ src/component/searchCard.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "../style.css";
 import { getMangaImg } from "../api";
 
 export default function searchCard(props) {
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    getMangaImg(props.id).then((e) => setImage(e.image));
+  }, []);
+
   return (
     <React.StrictMode>
       <div className="search-card" key={props.key}>
         <a href="">
-          <img src={getMangaImg(props.id)} className="search-card-image"></img>
+          <img src={image} className="search-card-image" />
           <div className="search-card-title">
             <h2>{props.title}</h2>
           </div>
