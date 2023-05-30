64957fc1b4565b68aa03854d52319a1b437b3fdb
riojr100 <riojr100@gmail.com>
2023-05-30 10:00:12 +0700

    yes

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Footer from "../component/footer";
 import Header from "../component/header";
 import SearchCard from "../component/searchCard";
 import "../style.css";
